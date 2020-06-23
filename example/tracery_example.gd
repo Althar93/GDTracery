f6191894781625b8a8c45923b11f0f79394a896d
Craig Hupin <craig.hupin@yahoo.com>
2020-06-23 08:19:22 +0100

    Updated formatting of the example script

--- example/tracery_example.gd
+++ example/tracery_example.gd
@@ -8,11 +8,11 @@ func _ready():
 	
 	# A simple grammar - or read from a JSON file
 	var grammar_test = Dictionary()
-	grammar_test["sentence"] 	= ["A #colour# #animal.capitalize#"]
-	grammar_test["animal"] 		= ["dog", "cat", "mouse", "rat", "cow", "pig", "unicorn"]
-	grammar_test["colour"]		= ["#tone# #baseColour#"]
-	grammar_test["tone"] 		= ["dark", "light", "pale"]
-	grammar_test["baseColour"] 	= ["red", "green", "blue", "yellow"]
+	grammar_test["sentence"] = ["A #colour# #animal.capitalize#"]
+	grammar_test["animal"] = ["dog", "cat", "mouse", "rat", "cow", "pig", "unicorn"]
+	grammar_test["colour"] = ["#tone# #baseColour#"]
+	grammar_test["tone"] = ["dark", "light", "pale"]
+	grammar_test["baseColour"] = ["red", "green", "blue", "yellow"]
 	
 	# Create our grammar
 	var grammar = Tracery.Grammar.new( grammar_test )
@@ -21,9 +21,9 @@ func _ready():
 	grammar.rng = rng
 	
 	# Add the english modifiers
-	grammar.add_modifiers( Tracery.UniversalModifiers.get_modifiers() )
+	grammar.add_modifiers(Tracery.UniversalModifiers.get_modifiers())
 	
 	# Flatten / generate a couple of sentences
 	for i in range( 0, 5 ):
-		var sentence = grammar.flatten( "#sentence#" )
-		print( sentence )
+		var sentence = grammar.flatten("#sentence#")
+		print(sentence)
